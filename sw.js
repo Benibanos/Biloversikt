06cd3aa6462cd025754251bf303eafa350935063
Benibanos <benjamin.pedersen@bring.com>
2026-08-04 21:20:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //
 // VIKTIG VED OPPDATERING: øk CACHE_VERSION når index.html eller andre filer i
 // APP_SHELL endres, ellers kan brukere sitte fast på en gammel, cachet versjon.
-const CACHE_VERSION = 'bilpark-v6';
+const CACHE_VERSION = 'bilpark-v7';
 const APP_SHELL = [
   './',
   './index.html',
